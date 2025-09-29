a3c421a5a226dfefc5baea5de3c07a3193639dbb
Ycaro02 <ycaro02@hotmail.fr>
2025-09-29 19:23:39 +0200

    Add script and docker compose for complete test

--- rsc/sh/get_licriterion.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Snaipe/Criterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz
-tar -xvf criterion-2.4.2-linux-x86_64.tar.xz && rm -f criterion-2.4.2-linux-x86_64.tar.xz
-mkdir -p deps
-mv criterion-2.4.2 deps
